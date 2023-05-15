30fe5d5c0339cc4ff275a67efcd123a55eb903af
SEONGJE <seongje00416@gmail.com>
2023-05-15 15:28:33 +0900

    getAllReward 추가

--- src/Reward/RewardListImpl.java
+++ src/Reward/RewardListImpl.java
@@ -1,14 +1,14 @@
 package Reward;
 
+import java.util.Vector;
+
 public class RewardListImpl implements RewardList {
 	public Reward m_Reward;
+	public Vector<Reward> rewardList;
 
-	public RewardListImpl(){
-
-	}
-	public void finalize() throws Throwable {
-
-	}
+	public RewardListImpl(){ this.rewardList = new Vector<Reward>(); }
+	public void finalize() throws Throwable {}
+	
 	public void add( Reward newReward ){
 		// 먼저 엔티티를 완성하고 오면 여기서 추가하는 방법
 	}
@@ -16,11 +16,11 @@ public class RewardListImpl implements RewardList {
 		// 삭제할 엔티티의 ID를 입력받아 삭제
 	}
 	public Reward retrieve(){
-		// case 1. 하나씩 가져올지 -> return Reward
-		// case 2. 전부 가져올지 -> return Vector<Reward>
+		// rewardList에 모든 데이터를 끌어옴 ( 근데 이러면 너무 큰 데이터를 들고 움직여야 하지 않을까... )
 		return null;
 	}
 	public void update( Reward reward ){
 		// 받은 Reward의 ID를 확인해 기존 Reward와 변경
 	}
+	public Vector<Reward> getAllReward(){ return this.rewardList; }
 }
